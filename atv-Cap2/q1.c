bbd11ff1c5079b9c1931e7dce68397fe99017c59
amandasantos312 <catce.2022111tads0053@aluno.ifpi.edu.br>
2025-09-18 14:27:06 -0300

    q1 de pilhas

---
+++ atv-Cap2/q1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "Pilha.h"
+
+int main() {
+    Pilha P = pilha(3);
+    empilha(1, P);
+    empilha(2, P);
+    printf("%d\n", desempilha(P));
+    printf("%d\n", desempilha(P));
+    printf("%d\n", desempilha(P));
+
+    return 0;
+}
